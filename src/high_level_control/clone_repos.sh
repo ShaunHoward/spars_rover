9a6371651c508f54357f329aa63db966b14ff09b
Shaun Howard <smh150@case.edu>
2016-09-02 01:22:48 -0400

    removed base robot files

---
+++ src/high_level_control/clone_repos.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/tu-darmstadt-ros-pkg/hector_gazebo.git &
+
+git clone https://github.com/tu-darmstadt-ros-pkg/hector_tracked_vehicles_common.git &
+
+git clone https://github.com/ros-perception/openslam_gmapping &
+
+git clone https://github.com/ros-perception/slam_gmapping &
